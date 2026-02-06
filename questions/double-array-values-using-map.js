d696508cd39a83f3f75e3c6fa28aaf24e7b253de
Sagar1329 <sagargowda1329@gmail.com>
2026-02-07 00:14:36 +0530

    implement double-array-values-using-map.js

---
+++ questions/double-array-values-using-map.js
@@ -0,0 +1,7 @@
+// Auto-generated from questions.js
+
+// === QUESTION: double array values using map ===
+const arr = [2, 5, 8, 2, 43];
+const doubled = arr.map(num => num * 2);
+console.log(doubled);
+
