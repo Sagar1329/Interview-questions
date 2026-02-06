81f4633891ee671d79b6d0dd9671a6951a19afd9
Sagar1329 <sagargowda1329@gmail.com>
2026-02-07 00:11:42 +0530

    implement question_13.js

---
+++ questions/question_13.js
@@ -0,0 +1,10 @@
+// Auto-generated file
+
+// === QUESTION: convert object to array ===
+const userObj = {
+    1: { id: 1, name: "Alice" },
+    2: { id: 2, name: "Bob" }
+};
+
+console.log(Object.values(userObj));
+
