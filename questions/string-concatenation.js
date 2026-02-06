30137a76e16ff5bab55511b6967484775edbcde8
Sagar1329 <sagargowda1329@gmail.com>
2026-02-07 00:14:39 +0530

    implement string-concatenation.js

---
+++ questions/string-concatenation.js
@@ -0,0 +1,8 @@
+// Auto-generated from questions.js
+
+// === QUESTION: string concatenation ===
+const str1 = "Hello";
+const str2 = "World";
+console.log(str1 + " " + str2);
+console.log(str1.concat(" ", str2));
+
