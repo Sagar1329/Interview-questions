8c43fe05530a6b14495b0df645f1be84c7b3ee10
Sagar1329 <sagargowda1329@gmail.com>
2026-02-07 00:11:38 +0530

    implement question_04.js

---
+++ questions/question_04.js
@@ -0,0 +1,13 @@
+// Auto-generated file
+
+// === QUESTION: flatten nested array ===
+const flatArray = [1, [2, 3], [4, [5]]];
+function flattenArray(array) {
+    return array.reduce(
+        (acc, cur) =>
+            Array.isArray(cur) ? acc.concat(flattenArray(cur)) : acc.concat(cur),
+        []
+    );
+}
+console.log(flattenArray(flatArray));
+
