c1bec962d73bf6360c4b5d88a632b6bc403545b3
Sagar1329 <sagargowda1329@gmail.com>
2026-02-07 00:11:40 +0530

    implement question_09.js

---
+++ questions/question_09.js
@@ -0,0 +1,9 @@
+// Auto-generated file
+
+// === QUESTION: get names of users above 25 using reduce ===
+const namesAbove25 = users.reduce((acc, user) => {
+    if (user.age > 25) acc.push(user.name);
+    return acc;
+}, []);
+console.log(namesAbove25);
+
