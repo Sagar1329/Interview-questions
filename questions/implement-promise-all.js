531648bcc6bded84c6a585540875df45a88b69a1
Sagar1329 <sagargowda1329@gmail.com>
2026-02-08 22:23:58 +0530

    implement implement-promise-all.js

---
+++ questions/implement-promise-all.js
@@ -0,0 +1,21 @@
+// Auto-generated from transformations.js
+
+function promiseAll(promises) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        let completed = 0;
+
+        promises.forEach((promise, index) => {
+            Promise.resolve(promise)
+                .then(value => {
+                    results[index] = value;
+                    completed++;
+                    if (completed === promises.length) resolve(results);
+                })
+                .catch(reject);
+        });
+    });
+}
+
+promiseAll([Promise.resolve(1), Promise.resolve(2)]).then(console.log);
+
