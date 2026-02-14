c5170a2edf1c7b8db126a5e7c553626a1771728d
Sagar1329 <sagargowda1329@gmail.com>
2026-02-14 23:10:46 +0530

    implement retry-promise-function.js

---
+++ questions/retry-promise-function.js
@@ -0,0 +1,12 @@
+// Auto-generated from transformations.js
+
+async function retry(fn, retries = 3) {
+  try {
+    return await fn();
+  } catch (err) {
+    if (retries === 0) throw err;
+    return retry(fn, retries - 1);
+  }
+}
+
+
