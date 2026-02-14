cb436d0e489a60572c109fbb857e9312d0ae8fd8
Sagar1329 <sagargowda1329@gmail.com>
2026-02-14 23:10:42 +0530

    implement implement-once-function.js

---
+++ questions/implement-once-function.js
@@ -0,0 +1,19 @@
+// Auto-generated from transformations.js
+
+function once(fn) {
+  let called = false;
+  let result;
+  return function (...args) {
+    if (!called) {
+      called = true;
+      result = fn.apply(this, args);
+    }
+    return result;
+  };
+}
+
+const initialize = once(() => "Initialized");
+console.log(initialize());
+console.log(initialize());
+
+
