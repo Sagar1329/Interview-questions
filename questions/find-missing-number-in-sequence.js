7e252c9ce38ef14a2cbc6759302ba3bd7fd8ff6a
Sagar1329 <sagargowda1329@gmail.com>
2026-02-14 23:10:44 +0530

    implement find-missing-number-in-sequence.js

---
+++ questions/find-missing-number-in-sequence.js
@@ -0,0 +1,11 @@
+// Auto-generated from transformations.js
+
+function findMissing(arr) {
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = arr.reduce((a, b) => a + b, 0);
+  return expectedSum - actualSum;
+}
+console.log(findMissing([1,2,4,5]));
+
+
