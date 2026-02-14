5676f7aa99d1717d470173c71ba1cb72d24aef82
Sagar1329 <sagargowda1329@gmail.com>
2026-02-14 23:10:43 +0530

    implement check-palindrome-string.js

---
+++ questions/check-palindrome-string.js
@@ -0,0 +1,9 @@
+// Auto-generated from transformations.js
+
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+console.log(isPalindrome("madam"));
+
+
