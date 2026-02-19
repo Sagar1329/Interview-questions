281c58d529d4c916f427c3d87b582bcd14848b55
Sagar1329 <sagargowda1329@gmail.com>
2026-02-19 23:58:30 +0530

    implement find-longest-substring-without-repeating-characters.js

---
+++ questions/find-longest-substring-without-repeating-characters.js
@@ -0,0 +1,16 @@
+// Auto-generated from transformations.js
+
+function longestSubstring(str) {
+  let set = new Set(), left = 0, max = 0;
+  for (let right = 0; right < str.length; right++) {
+    while (set.has(str[right])) {
+      set.delete(str[left++]);
+    }
+    set.add(str[right]);
+    max = Math.max(max, right - left + 1);
+  }
+  return max;
+}
+console.log(longestSubstring("abcabcbb"));
+
+
