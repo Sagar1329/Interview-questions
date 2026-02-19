756ffe8cf6a2af110adebd634d3342a0dbea338c
Sagar1329 <sagargowda1329@gmail.com>
2026-02-19 23:58:29 +0530

    implement generate-fibonacci-sequence.js

---
+++ questions/generate-fibonacci-sequence.js
@@ -0,0 +1,12 @@
+// Auto-generated from transformations.js
+
+function fibonacci(n) {
+  const seq = [0, 1];
+  for (let i = 2; i < n; i++) {
+    seq.push(seq[i - 1] + seq[i - 2]);
+  }
+  return seq.slice(0, n);
+}
+console.log(fibonacci(6));
+
+
