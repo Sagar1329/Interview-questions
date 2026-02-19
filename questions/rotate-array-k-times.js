ac7ab756bc1138d9b7a0bcb9750b47d1401d0ecf
Sagar1329 <sagargowda1329@gmail.com>
2026-02-19 23:58:29 +0530

    implement rotate-array-k-times.js

---
+++ questions/rotate-array-k-times.js
@@ -0,0 +1,9 @@
+// Auto-generated from transformations.js
+
+function rotateArray(arr, k) {
+  k %= arr.length;
+  return [...arr.slice(-k), ...arr.slice(0, -k)];
+}
+console.log(rotateArray([1, 2, 3, 4, 5], 2));
+
+
